7364530997a108ef08454ddf37fa245c7d970d9f
ILYA KOPYTOV <kopytov.ilya.rus@mail.ru>
2023-03-13 21:38:07 +0300

    Added WebDriverConfig & WebDriverProvider

---
+++ src/test/java/config/WebDriverProvider.java
@@ -0,0 +1,42 @@
+package config;
+
+import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import tests.TestBase;
+
+import java.util.Map;
+
+public class WebDriverProvider extends TestBase {
+
+    private WebDriverConfig config;
+
+    public WebDriverProvider() {
+        this.config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+        createWebDriver();
+    }
+
+    private void createWebDriver() {
+        switch (config.getBrowser().toLowerCase()) {
+            case "chrome":
+                Configuration.browser = "chrome";
+                break;
+            case "firefox":
+                Configuration.browser = "firefox";
+                break;
+            default:
+                throw new RuntimeException(config.getBrowser());
+        }
+        Configuration.baseUrl = config.getBaseUrl();
+        Configuration.browserVersion = config.getBrowserVersion();
+        Configuration.remote = config.getRemoteUrl();
+        Configuration.browserSize = config.getBrowserSize();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverProvider;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,17 +24,19 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://www.tinkoff.ru/";
-        Configuration.browserSize = System.getProperty("resolution", "1920x1080");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("version", "100.0");
-        Configuration.remote = System.getProperty("remote");
-        Configuration.holdBrowserOpen = true;
+//        Configuration.baseUrl = "https://www.tinkoff.ru/";
+//        Configuration.browserSize = System.getProperty("resolution", "1920x1080");
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//        Configuration.browserVersion = System.getProperty("version", "100.0");
+//        Configuration.remote = System.getProperty("remote");
+//        Configuration.holdBrowserOpen = true;
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        WebDriverProvider provider = new WebDriverProvider();
+
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
